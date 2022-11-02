072310a895c4c852e52a605aa0c0507cb031d5e2
honcharov404 <111276494+honcharov404@users.noreply.github.com>
2022-11-02 11:47:35 +0000

    Deploying to gh-pages from @ honcharov404/goit-js-hw-09@b10970d112231e86cc144eb9d04ae7126ea0ee44 🚀

---
+++ 01-color-switcher.51392222.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");let o=null;function r(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){o=setInterval(r,1e3),e.setAttribute("disabled","disabled")})),n.addEventListener("click",(function(){clearInterval(o),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.51392222.js.map
