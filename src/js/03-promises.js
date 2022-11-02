0ac1e90333bca6c1ff9f9d285916e213ce61673b
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-11-02 16:28:07 +0200

    task-3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const elements = e.currentTarget.elements;
+  const delayNumber = Number(elements.delay.value);
+  const stepNumber = Number(elements.step.value);
+  const amountNumber = Number(elements.amount.value);
+  let delay = 0;
+  let position = 0;
+
+  for (let i = 0; i < amountNumber; i += 1) {
+    position += 1;
+    delay = delayNumber + stepNumber * i;
+
+    createPromise(position, delay)
+      .then(resolve => Notiflix.Notify.success(resolve))
+      .catch(reject => Notiflix.Notify.failure(reject));
+  }
+  evt.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    });
+  });
 }
