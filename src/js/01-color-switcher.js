b10970d112231e86cc144eb9d04ae7126ea0ee44
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-11-02 13:47:02 +0200

    task-1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  timerId = setInterval(onChangeColor, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+}
+
+function onChangeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopChangeColor() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
